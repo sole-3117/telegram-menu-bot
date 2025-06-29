b5d6329fe1b514d6b5536b6c3ea0fc5b39009526
sole-3117 <solejonadashov0@gmail.com>
2025-06-29 15:17:42 +0500

    Update user_manager.py

--- user_manager.py
+++ user_manager.py
@@ -5,29 +5,29 @@ DB_FILE = "db.json"
 
 def load_db():
     if not os.path.exists(DB_FILE):
-        return {"users": {}, "tokens": {}, "limits": {}, "buttons": {}}
+        with open(DB_FILE, "w") as f:
+            json.dump({}, f)
     with open(DB_FILE, "r") as f:
         return json.load(f)
 
-def save_db(db):
+def save_db(data):
     with open(DB_FILE, "w") as f:
-        json.dump(db, f, indent=4)
+        json.dump(data, f, indent=2)
 
-def handle_user_start(bot, message, ADMIN_ID):
+def handle_user_start(bot, message, admin_id):
     user_id = str(message.from_user.id)
     name = message.from_user.first_name
     db = load_db()
-    if user_id not in db["users"]:
-        db["users"][user_id] = {"name": name}
+    if user_id not in db:
+        db[user_id] = {"bots": []}
         save_db(db)
-        bot.send_message(ADMIN_ID, f"🆕 Yangi foydalanuvchi: {name} ({user_id})")
-    bot.send_message(message.chat.id, f"👋 Salom {name}! Botga xush kelibsiz.Token yuborib yangi bot qo‘shishingiz mumkin.")
+        bot.send_message(admin_id, f"🆕 Yangi foydalanuvchi: {name} ({user_id})")
+    bot.send_message(message.chat.id, f"👋 Salom {name}! Botga xush kelibsiz.")
 
 def handle_admin_message(bot, text):
     try:
-        parts = text.split(" ", 2)
-        user_id = parts[1]
-        reply_text = parts[2]
-        bot.send_message(user_id, f"📩 Admin javobi:{reply_text}")
+        _, uid, msg = text.split(" ", 2)
+        bot.send_message(int(uid), f"📩 Admin javobi:
+{msg}")
     except:
         pass
