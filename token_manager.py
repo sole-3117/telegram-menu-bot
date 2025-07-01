43f5616b675d8965a7375115e03eb2064dfbfe50
sole-3117 <solejonadashov0@gmail.com>
2025-07-01 23:16:06 +0500

    Update token_manager.py

--- token_manager.py
+++ token_manager.py
@@ -4,16 +4,20 @@ def handle_token_input(bot, message, admin_id):
     user_id = str(message.from_user.id)
     token = message.text.strip()
     db = load_db()
+
     if user_id not in db:
-        db[user_id] = {"bots": []}
+        return
+
     if token in db[user_id]["bots"]:
         bot.send_message(message.chat.id, "❗ Bu token allaqachon qo‘shilgan.")
         return
-    if len(db[user_id]["bots"]) >= 1:
-        bot.send_message(message.chat.id, "🚫 Faqat 1ta bot bepul qo‘shilishi mumkin. Ko‘proq bot qo‘shish uchun adminga murojaat qiling.")
-        bot.send_message(admin_id, f"💰 {user_id} yangi bot qo‘shmoqchi: {token}")
+
+    if len(db[user_id]["bots"]) >= db[user_id].get("bot_limit", 1):
+        bot.send_message(message.chat.id, "🚫 Limit tugagan. Yana bot qo‘shish uchun adminga murojaat qiling.")
+        bot.send_message(admin_id, f"💰 {user_id} yana bot qo‘shmoqchi: {token}")
         return
+
     db[user_id]["bots"].append(token)
     save_db(db)
-    bot.send_message(message.chat.id, "✅ Bot token qabul qilindi va qo‘shildi.")
-    bot.send_message(admin_id, f"➕ Yangi bot qo‘shildi: {token}👤 {user_id}")
+    bot.send_message(message.chat.id, "✅ Bot token qabul qilindi.")
+    bot.send_message(admin_id, f"➕ Yangi bot qo‘shildi: {token}\n👤 {user_id}")
